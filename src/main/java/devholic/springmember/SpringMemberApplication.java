ad90733c6fa8811dc0c401d49d62e07b63705eef
devholic22 <hyunjoon.tech@gmail.com>
2024-02-08 13:18:04 +0900

    initial commit

---
+++ src/main/java/devholic/springmember/SpringMemberApplication.java
@@ -0,0 +1,13 @@
+package devholic.springmember;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMemberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMemberApplication.class, args);
+	}
+
+}
